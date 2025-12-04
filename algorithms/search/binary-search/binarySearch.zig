9cea878e448b545d8ce062f108728d60fd0df1b3
dajneem23 <dajneem23@gmail.com>
2025-12-04 11:27:47 +0700

    feat: Implement algorithms checklist and refactor binary search
    - Created ALGORITHMS.md to track algorithm implementations, based on
      trekhleb/javascript-algorithms.
    - Refactored  to be generic over
      and use a half-open interval approach, similar to the JavaScript reference.
    - Updated main function to use  for output.
    - Marked Binary Search as completed in ALGORITHMS.md.

--- algorithms/search/binary-search/binarySearch.zig
+++ algorithms/search/binary-search/binarySearch.zig
@@ -1,22 +1,28 @@
 const std = @import("std");
 
-pub fn binarySearch(sortedArray: []u32, seekElement: u32) !u32 {
-    var n = sortedArray.len;
-    var i: usize = 0;
+/// Binary search implementation.
+/// Returns the index of the element if found, or null if not found.
+/// Assumes the array is sorted in ascending order.
+pub fn binarySearch(comptime T: type, sortedArray: []const T, seekElement: T) ?usize {
+    var startIndex: usize = 0;
+    var endIndex: usize = sortedArray.len;
 
-    while (i < n) {
-        const middle = i + (n - i) / 2;
-        if (sortedArray[middle] == seekElement) {
-            return @intCast(middle);
+    while (startIndex < endIndex) {
+        const middleIndex = startIndex + (endIndex - startIndex) / 2;
+        const value = sortedArray[middleIndex];
+
+        if (value == seekElement) {
+            return middleIndex;
         }
 
-        if (sortedArray[middle] < seekElement) {
-            i = middle + 1;
+        if (value < seekElement) {
+            startIndex = middleIndex + 1;
         } else {
-            n = middle;
+            endIndex = middleIndex;
         }
     }
-    return error.notFound;
+
+    return null;
 }
 
 pub fn generateSortedRandomArray(allocator: std.mem.Allocator, count: usize) ![]u32 {
@@ -31,15 +37,27 @@ pub fn generateSortedRandomArray(allocator: std.mem.Allocator, count: usize) ![]
 }
 
 pub fn main() !void {
-    //const stdout = std.io.getStdOut().writer();
-    var stdout = std.fs.File.stdout().writer(&.{});
-
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
+
     const nums = try generateSortedRandomArray(allocator, 10);
-    try stdout.interface.print("Sorted numbers: {any}\n", .{nums});
-    const result = binarySearch(nums, nums[std.crypto.random.uintLessThan(usize, nums.len)]);
-    try stdout.interface.print("Found {any}\n ", .{result});
-    std.debug.assert(result != error.notFound);
+    std.debug.print("Sorted numbers: {any}\n", .{nums});
+
+    // Pick a random element to search for
+    const target = nums[std.crypto.random.uintLessThan(usize, nums.len)];
+    
+    if (binarySearch(u32, nums, target)) |index| {
+        std.debug.print("Found {} at index {}\n", .{target, index});
+    } else {
+        std.debug.print("Did not find {}\n", .{target});
+    }
+
+    // Test not found
+    const notFoundTarget = 99999999; 
+    if (binarySearch(u32, nums, notFoundTarget)) |index| {
+         std.debug.print("Found {} at index {}\n", .{notFoundTarget, index});
+    } else {
+         std.debug.print("Did not find {} (as expected, mostly)\n", .{notFoundTarget});
+    }
 }
